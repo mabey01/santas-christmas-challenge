709347ab264c989e9f241e6859ee5a6614eb3b35
Siobhan Hokin <skehokin@gmail.com>
2021-12-20 16:02:15 +1000

    oh dear

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,40 @@ const Ground = styled.div`
 
 export function App() {
   return (
-    <div className="relative isolate h-screen flex justify-center items-center text-xl">
+    <div className='relative isolate h-screen flex justify-center items-center text-xl'>
       <Snow />
 
-      <Ground className="absolute bottom-0 left-0 right-0 h-[20vh] bg-white" />
+      <Ground className='absolute bottom-0 left-0 right-0 h-[20vh] bg-white' />
 
-      <main className="absolute w-[50vw] h-[75vh] text-neutral-800">
-        <div className="h-full flex justify-center items-center text-center">
-          <div className="max-w-md">
-            Your challenge, should you decide to accept it, is to build a
-            <span className="text-rose-600 font-semibold"> Santa</span>
-          </div>
+      <main className='absolute w-[50vw] h-[75vh] text-neutral-800'>
+        <div className='head'></div>
+        <div className='head2'>
+          <div className='mouth'></div>
         </div>
+
+        <div className='nose'></div>
+        <div className='cheek1'></div>
+        <div className='cheek2'></div>
+
+        <div className='eye1'>
+          <div className='pupil' />
+        </div>
+        <div className='eye2'>
+          <div className='pupil' />
+        </div>
+        <div className='beard1' />
+        <div className='beard2' />
+
+        <div className='body'>
+          <div className='body2' />
+        </div>
+        <div className='legs'></div>
+
+        <div className='leg-divider'></div>
+
+        <div className='shoe1'></div>
+        <div className='shoe2'></div>
+        <div className='hat'></div>
       </main>
 
       <Target />
