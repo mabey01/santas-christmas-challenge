f60b26978bedfdec4a1d62cfbf439ae2e32b2a0b
Maximilian Beyer <mabey01@gmail.com>
2021-12-20 10:49:00 +1000

    feat: add opacity to target

--- src/target.tsx
+++ src/target.tsx
@@ -1,12 +1,21 @@
 import { useEffect, useState } from "react";
 import Santa from "./assets/santa.png";
 
+const OPACITY = 0.4;
+
 export function Target() {
   const [show, setShowTarget] = useState(false);
+  const [opacity, setOpacity] = useState(1);
 
   useEffect(() => {
     function handleKeypress(e: KeyboardEvent) {
-      if (e.key === "s") {
+      if (e.key === "s" && !e.ctrlKey) {
+        setOpacity(OPACITY);
+        setShowTarget((show) => !show);
+      }
+
+      if (e.key === "s" && e.ctrlKey) {
+        setOpacity(1);
         setShowTarget((show) => !show);
       }
     }
@@ -21,7 +30,11 @@ export function Target() {
 
   return (
     <div className="absolute w-[50vw] h-[75vh]">
-      <img src={Santa} className="w-full h-full object-contain" />
+      <img
+        src={Santa}
+        className="w-full h-full object-contain"
+        style={{ opacity: opacity }}
+      />
     </div>
   );
 }
